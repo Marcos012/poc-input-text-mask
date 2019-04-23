d77ed1654e3685a585ccfdb87d905a82c74f96ba
marcos_lara <marcos_lara@terceiros.sicredi.com.br>
2019-04-23 08:37:27 -0300

    Adiciona campo de data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import createNumberMask from 'text-mask-addons/dist/createNumberMask';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 
 
 export class AppComponent {
   public myModel = '';
-  public mask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
+  public telephoneMask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
+  public dateMask = [/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/];
 
   numberMask = createNumberMask({
     prefix: '',
